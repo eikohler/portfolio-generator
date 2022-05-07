b122c04be7326b6863ab7534e55f77a284b9fb77
Eric Kohler <erickohler1600@gmail.com>
2022-05-07 18:20:43 -0400

    finalize html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
@@ -176,22 +176,40 @@ Add a New Project
   });
 };
 
-const pageHTML = generatePage(mockData);
+// const pageHTML = generatePage(mockData);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./dist/index.html', pageHTML, err => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
 
-  console.log('Page created! Check out index.html in this directory to see it!');
-});
+//   console.log('Page created! Check out index.html in this directory to see it!');
 
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     const pageHTML = generatePage(portfolioData);
-    
-//     fs.writeFile('./index.html', pageHTML, err => {
-//       if (err) throw new Error(err);
-
-//       console.log('Page created! Check out index.html in this directory to see it!');
-//     });
+//   fs.copyFile('./src/style.css', './dist/style.css', err => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log('Style sheet copied successfully!');
+//   });
 // });
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
